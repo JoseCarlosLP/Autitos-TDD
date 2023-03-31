92f42fd61e71beefc8ac4f4a4a80b545ef133071
Jose Carlos Lozada Peralta <jose.lozada@ucb.edu.bo>
2023-03-31 11:19:14 -0400

    Se ingresa el comando A para avanzar una sola vez y se procesa el movimiento

--- src/ControladorAuto.js
+++ src/ControladorAuto.js
@@ -1,3 +1,6 @@
+const posicionInicial="0,0N";
+
 export function ejecutar(comando){
-    return "0,0N";
+    if(comando==="") return posicionInicial;
+    return "0,1N";
 }
--- src/ControladorAuto.spec.js
+++ src/ControladorAuto.spec.js
@@ -6,5 +6,8 @@ describe("Comandos de movimiento del Auto", () => {
     });
     it("No se ingreso un comando", () => {
         expect(ejecutar("")).toEqual("0,0N");
-      });
+    });
+    it("Se ingresa el comando A para avanzar", () => {
+        expect(ejecutar("A")).toEqual("0,1N");
+    });
  });
