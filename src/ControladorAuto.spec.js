5007d169ca7a24d22cda7dcbdc42c69319c14a09
Adrian Montano <adrian.montano2011@yahoo.com>
2023-04-09 21:49:36 -0400

    Si la cadena de comando es DD, gira dos veces a la derecha retornando la direccion S

--- src/ControladorAuto.spec.js
+++ src/ControladorAuto.spec.js
@@ -1,24 +1,34 @@
 import Auto from "./class-auto.js"
 
 describe("Comandos de movimiento del Auto", () => {
-    const auto = new Auto();
     it("", () => {
+      const auto = new Auto();
       expect(true).toEqual(true);
     });
     it("No se ingreso un comando", () => {
+        const auto = new Auto();
         expect(auto.ejecutar("")).toEqual("0,0N");
     });
     it("Se ingresa el comando A para avanzar", () => {
+        const auto = new Auto();
         expect(auto.ejecutar("A")).toEqual("0,1N");
     });
     it("Se ingresa el comando AA para avanzar 2 veces", () => {
+        const auto = new Auto();
         expect(auto.ejecutar("AA")).toEqual("0,2N");
     });
     it("Se ingresa el comando A repetidas veces", () => {
+        const auto = new Auto();
         expect(auto.ejecutar("AAAA")).toEqual("0,4N");
-        expect(auto.ejecutar("AAAAAAA")).toEqual("0,7N");
+        const auto2 = new Auto();
+        expect(auto2.ejecutar("AAAAAAA")).toEqual("0,7N");
     });
     it("Se ingresa el comando D para girar a la derecha", () => {
+        const auto = new Auto();
         expect(auto.ejecutar("D")).toEqual("0,0E");
+    });
+    it("Se ingresa el comando D para girar a la derecha", () => {
+        const auto = new Auto();
+        expect(auto.ejecutar("DD")).toEqual("0,0S");
     });
  });
--- src/class-auto.js
+++ src/class-auto.js
@@ -4,24 +4,39 @@ class Auto
     {
         this.posicionInicialX = 0;
         this.posicionInicialY = 0;
+        this.posicionActualX = this.posicionInicialX;
+        this.posicionActualY = this.posicionInicialY;
+        this.orientacionInicial = "N";
+        this.orientacionActual = this.orientacionInicial;
     }
 
     ejecutar(cadenaComando){
-        let posicionActualX=this.posicionInicialX;
-        let posicionActualY=this.posicionInicialY;
-        if(cadenaComando=="D")
-        return posicionActualX+","+posicionActualY+"E"; 
-        else
-            posicionActualY=this.moverAuto(cadenaComando,posicionActualY);
-        return posicionActualX+","+posicionActualY+"N";
+        this.moverAuto(cadenaComando);
+        return this.posicionActualX+","+this.posicionActualY+this.orientacionActual;
     }
 
-    moverAuto(cadenaComando,posicionActualY){
+    moverAuto(cadenaComando){
         for(let comando=0;comando<cadenaComando.length;comando++){
-            posicionActualY++;
+            if(cadenaComando[comando]=="D")
+            {
+                this.rotarDerecha();
+            }
+            else
+                this.posicionActualY++;
         }
-        return posicionActualY;
-    }   
+    }
+
+    rotarDerecha()
+    {
+        if(this.orientacionActual=="N")
+        {
+            this.orientacionActual="E";
+        }
+        else if(this.orientacionActual=="E")
+        {
+            this.orientacionActual="S";
+        }
+    }
 }
 
 export default Auto;
