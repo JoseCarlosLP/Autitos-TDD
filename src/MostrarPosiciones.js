85d4925bda26c8592fa0a1f2630bbe4469910ea4
Adrian Montano <adrian.montano2011@yahoo.com>
2023-04-10 17:43:44 -0400

    Se actualizó MostrarPosiciones instanciando la clase auto para poder observar los resultados esperados en el presenter

--- src/MostrarPosiciones.js
+++ src/MostrarPosiciones.js
@@ -1,9 +1,10 @@
-import { ejecutar } from "./class-auto";
+import Auto from "./class-auto";
 
 export function mostrarPosiciones(cadenaComando)
 {
+    const auto = new Auto();
     let div = document.querySelector("#posiciones");
-    div.innerHTML="Posicion Inicial = "+ ejecutar("") + "<br> " + 
+    div.innerHTML="Posicion Inicial = "+ auto.ejecutar("") + "<br> " + 
     "Comandos: "+cadenaComando + "<br> " + 
-    "Posicion Final : "+ejecutar(cadenaComando);
+    "Posicion Final = "+ auto.ejecutar(cadenaComando);
 }
