3dfa9d4122ce5e3ff814b40dc67ed8d2d4cace93
Jose Carlos Lozada Peralta <jose.lozada@ucb.edu.bo>
2023-04-09 22:37:48 -0400

    Se limpio el codigo de la clase Auto para mayor claridad y comprension

--- src/class-auto.js
+++ src/class-auto.js
@@ -20,77 +20,34 @@ class Auto
 
     moverAuto(cadenaComando){
         for(let comando=0;comando<cadenaComando.length;comando++){
-            if(cadenaComando[comando]=="D")
-            {
-                this.rotarDerecha();
-            }
-            else if(cadenaComando[comando]=="A")
-            {
-                this.avanzar();
-            }
+            if(cadenaComando[comando]=="D") this.rotarDerecha();
+            else this.avanzar();
         }
     }
 
     verificarLimites()
     {
-        if(this.posicionActualX==limiteX)
-        {
-            this.posicionActualX--;
-        }
-        if(this.posicionActualX==-1)
-        {
-            this.posicionActualX++;
-        }
-        if(this.posicionActualY==limiteY)
-        {
-            this.posicionActualY--;
-        }
-        if(this.posicionActualY==-1)
-        {
-            this.posicionActualY++;
-        }
+        if(this.posicionActualX==limiteX) this.posicionActualX--;
+        if(this.posicionActualX==-1) this.posicionActualX++;        
+        if(this.posicionActualY==limiteY) this.posicionActualY--;
+        if(this.posicionActualY==-1) this.posicionActualY++;
     }
 
     avanzar()
     {
-        if(this.orientacionActual=="N")
-        {
-                this.posicionActualY++;
-        }
-        else if(this.orientacionActual=="E")
-        {
-                this.posicionActualX++;
-        }
-        else if(this.orientacionActual=="S")
-        {
-                this.posicionActualY--;
-        }
-        else if(this.orientacionActual=="O")
-        {
-                this.posicionActualX--;
-        }
+        if(this.orientacionActual=="N") this.posicionActualY++;
+        else if(this.orientacionActual=="E") this.posicionActualX++;
+        else if(this.orientacionActual=="S") this.posicionActualY--;
+        else this.posicionActualX--;
         this.verificarLimites();
     }
 
     rotarDerecha()
     {
-        if(this.orientacionActual=="N")
-        {
-            this.orientacionActual="E";
-        }
-        else if(this.orientacionActual=="E")
-        {
-            this.orientacionActual="S";
-        }
-        else if(this.orientacionActual=="S")
-        {
-            this.orientacionActual="O";
-        }
-        else if(this.orientacionActual=="O")
-        {
-            this.orientacionActual="N";
-        }
+        if(this.orientacionActual=="N")  this.orientacionActual="E";
+        else if(this.orientacionActual=="E") this.orientacionActual="S";
+        else if(this.orientacionActual=="S") this.orientacionActual="O";
+        else this.orientacionActual="N";
     }
 }
-
 export default Auto;
