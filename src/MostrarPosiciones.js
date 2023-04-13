07d5aa17d35b6b1269cd1046b3bb2b76ff4a51a0
Jose Carlos Lozada Peralta <jose.lozada@ucb.edu.bo>
2023-04-13 01:11:10 -0400

    Se actualizo el nombre de variable posicionesIniciales en la funcion mostrarPosiciones

--- src/MostrarPosiciones.js
+++ src/MostrarPosiciones.js
@@ -6,6 +6,7 @@ export function mostrarPosiciones(cadenaComando)
     let div = document.querySelector("#posiciones");
     posicionesIniciales.ejecutar(cadenaComando);
     const auto = new Auto();
-    div.innerHTML="Posicion Inicial = "+ auto.posicionInicialX+","+auto.posicionInicialY+auto.orientacionInicial + "<br> " + 
+    div.innerHTML="Posicion Inicial = "+ posicionesIniciales.posicionInicialX+","+
+    posicionesIniciales.posicionInicialY+posicionesIniciales.orientacionInicial + "<br> " + 
     "Comandos: "+ cadenaComando + "<br> " + "Posicion Final = "+ auto.ejecutar(cadenaComando);
 }
